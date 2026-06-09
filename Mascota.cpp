1c661cce1a61263e711cca97d05b4de570557aa1
A01710193 <a01710193@tec.mx>
2026-06-09 09:19:36 -0600

    Update Mascota.cpp

--- Mascota.cpp
+++ Mascota.cpp
@@ -1,8 +1,6 @@
 #include "Mascota.hpp"
-// -------- Mascota --------
 
-// Constructores y Destructor
-Mascota::Mascota(){
+Mascota::Mascota() {
     nombre = "";
     edad = 0;
     motivoConsulta = "";
@@ -10,9 +8,9 @@ Mascota::Mascota(){
     raza = "";
     cartillaVacunas = "";
     agresividad = false;
-};
+}
 
-Mascota::Mascota(string nNombre, int nEdad, string nMotivoConsulta, string nTipo, string nRaza, string nCartillaVacunas, bool nAgresividad){
+Mascota::Mascota(string nNombre, int nEdad, string nMotivoConsulta, string nTipo, string nRaza, string nCartillaVacunas, bool nAgresividad) {
     nombre = nNombre;
     edad = nEdad;
     motivoConsulta = nMotivoConsulta;
@@ -20,161 +18,71 @@ Mascota::Mascota(string nNombre, int nEdad, string nMotivoConsulta, string nTipo
     raza = nRaza;
     cartillaVacunas = nCartillaVacunas;
     agresividad = nAgresividad;
-};
+}
 
-Mascota::~Mascota(){};
+Mascota::~Mascota() {}
 
-// Setters
-void Mascota::setNombre(string nNombre){
-    nombre = nNombre;
-};
+void Mascota::setNombre(string nNombre) { nombre = nNombre; }
+void Mascota::setEdad(int nEdad) { edad = nEdad; }
+void Mascota::setMotivoConsulta(string nMotivoConsulta) { motivoConsulta = nMotivoConsulta; }
+void Mascota::setTipo(string nTipo) { tipo = nTipo; }
+void Mascota::setRaza(string nRaza) { raza = nRaza; }
+void Mascota::setCartillaVacunas(string nCartillaVacunas) { cartillaVacunas = nCartillaVacunas; }
+void Mascota::setAgresividad(bool nAgresividad) { agresividad = nAgresividad; }
 
-void Mascota::setEdad(int nEdad){
-    edad = nEdad;
-};
+string Mascota::GetNombre() { return nombre; }
+int Mascota::getEdad() { return edad; }
+string Mascota::getMotivoConsulta() { return motivoConsulta; }
+string Mascota::getTipo() { return tipo; }
+string Mascota::getRaza() { return raza; }
+string Mascota::getCartillaVacunas() { return cartillaVacunas; }
+bool Mascota::getAgresividad() { return agresividad; }
 
-void Mascota::setMotivoConsulta(string nMotivoConsulta){
-    motivoConsulta = nMotivoConsulta;
-};
+int Mascota::tipoConsulta() { return 1; }
+string Mascota::registrarCartilla() { return cartillaVacunas; }
 
-void Mascota::setTipo(string nTipo){
-    tipo = nTipo;
-};
-
-void Mascota::setRaza(string nRaza){
-    raza = nRaza;
-};
-
-void Mascota::setCartillaVacunas(string nCartillaVacunas){
-    cartillaVacunas = nCartillaVacunas;
-};
-
-void Mascota::setAgresividad(bool nAgresividad){
-    agresividad = nAgresividad;
-};
-
-// Getters
-string Mascota::GetNombre(){
-    return nombre;
-};
-
-int Mascota::getEdad(){
-    return edad;
-};
-
-string Mascota::getMotivoConsulta(){
-    return motivoConsulta;
-};
-
-string Mascota::getTipo(){
-    return tipo;
-};
-
-string Mascota::getRaza(){
-    return raza;
-};
-
-string Mascota::getCartillaVacunas(){
-    return cartillaVacunas;
-};
-
-bool Mascota::getAgresividad(){
-    return agresividad;
-};
-
-// Métodos
-int Mascota::tipoConsulta(){
-    if (motivoConsulta == "Revisión") return 1;
-    if (motivoConsulta == "Vacunación") return 2;
-    if (motivoConsulta == "Grooming") return 3;
-    if (motivoConsulta == "Emergencia") return 4;
-    return 0;
-};
-
-string Mascota::registrarCartilla(){
-    if (cartillaVacunas == ""){
-        cout << "Registrando nueva cartilla de vacunas para " << nombre << endl;
-        cartillaVacunas = "Cartilla de " + nombre + "\nNinguna vacuna registrada";
-        cout << "\n" << cartillaVacunas << endl;
-        return cartillaVacunas;
+void Mascota::agregarVacuna(string nVacuna) {
+    if (cartillaVacunas == "") {
+        cartillaVacunas = nVacuna;
+    } else {
+        cartillaVacunas += ", " + nVacuna;
     }
-    cout << "La cartilla a nombre de " << nombre << " ya está registrada :)" << endl;
-    return cartillaVacunas;
-};
+}
 
-void Mascota::agregarVacuna(string nuevaVacuna){
-    if (cartillaVacunas == ""){
-        cartillaVacunas = nuevaVacuna;
-    }
-    
-    else{
-        cartillaVacunas += cartillaVacunas + ", " + nuevaVacuna;
-        cout << "Vacuna " << nuevaVacuna << " ya fue agregada a la cartilla." << endl;
-    }
-};
+Perro::Perro() : Mascota() { tamaño = ""; }
+Perro::Perro(string nTamaño) : Mascota() { tamaño = nTamaño; }
+Perro::~Perro() {}
 
-// -------- Perro --------
+void Perro::setTamaño(string nTamaño) { tamaño = nTamaño; }
+string Perro::getTamaño() { return tamaño; }
 
-// Constructores y destructor
-Perro::Perro() : Mascota(){
-    tamaño = "";
-};
-
-Perro::Perro(string nTamaño) : Mascota(){
-    tamaño = nTamaño;
-};
-
-Perro::~Perro(){};
-
-// Setters
-void Perro::setTamaño(string nTamaño){
-    tamaño = nTamaño;
-};
-
-// Getters
-string Perro::getTamaño(){
-    return tamaño;
-};
-
-// Métodos
-void Perro::mostrarCuidados(){
+void Perro::mostrarCuidados() {
     cout << "------ Cuidados para Perro -------" << endl;
     cout << "Nombre: " << nombre << endl;
     cout << "Raza: " << raza << endl;
     cout << "Tamaño: " << tamaño << endl;
+    cout << "Vacunas: " << (cartillaVacunas == "" ? "Ninguna" : cartillaVacunas) << endl;
     cout << "Requiere paseos diarios, una buena alimentación diaria y mucha agua." << endl;
-    if (agresividad) cout << "Cuidado con el perro, es agresivo. Va a requerir usar un bozal para consultas" << endl;
-};
+    if (agresividad) {
+        cout << "Cuidado con el perro, es agresivo. Va a requerir usar un bozal para consultas" << endl;
+    }
+}
 
-// -------- Gato --------
+Gato::Gato() : Mascota() { actividadExterior = false; }
+Gato::Gato(bool nActividadExterior) : Mascota() { actividadExterior = nActividadExterior; }
+Gato::~Gato() {}
 
-// Constructores y destructor
-Gato::Gato() : Mascota(){
-    actividadExterior = false;
-};
+void Gato::setActividadExterior(bool nActividadExterior) { actividadExterior = nActividadExterior; }
+bool Gato::getActividadExterior() { return actividadExterior; }
 
-Gato::Gato(bool nActividadExterior) : Mascota(){
-    actividadExterior = nActividadExterior;
-};
-
-Gato::~Gato(){};
-
-// Setters
-void Gato::setActividadExterior(bool nActividadExterior){
-    actividadExterior = nActividadExterior;
-};
-
-// Getters
-bool Gato::getActividadExterior(){
-    return actividadExterior;
-};
-
-// Métodos
-void Gato::mostrarCuidados(){
+void Gato::mostrarCuidados() {
     cout << "------ Cuidados para Gato -------" << endl;
     cout << "Nombre: " << nombre << endl;
     cout << "Raza: " << raza << endl;
-    cout << "¿Sale al exterior? " << (actividadExterior ? "Si" : "No") << endl;
-    cout << "Requiere muchas caricias, una buena alimentación diaria y mucha agua." << endl;
-    if (agresividad) cout << "Cuidado con el gato, es agresivo. Va a requerir utilizar una jaula para consultas" << endl;
+    cout << "¿Sale al exterior? " << (actividadExterior ? "Sí" : "No") << endl;
+    cout << "Vacunas: " << (cartillaVacunas == "" ? "Ninguna" : cartillaVacunas) << endl;
+    cout << "Requiere rascadores, caja de arena limpia y estimulación en interiores." << endl;
+    if (agresividad) {
+        cout << "Cuidado con el gato, es agresivo. Manejar con precaución durante la consulta." << endl;
+    }
 }
