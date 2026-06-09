2c63eeeae8f18317e2a9e80bd6c5e7b8a80de542
A01710193 <a01710193@tec.mx>
2026-06-09 09:20:02 -0600

    Update Persona.hpp

--- Persona.hpp
+++ Persona.hpp
@@ -2,13 +2,13 @@
 #define Persona_hpp
 
 #include <iostream>
-#include "Mascota.hpp"
 #include <vector>
-
+#include <string>
+#include "Mascota.hpp"
 
 using namespace std;
 
-class Persona{
+class Persona {
 protected:
     string nombre;
     int edad;
@@ -17,40 +17,33 @@ protected:
     string direccion;
     
 public:
-    // Constructores y destructores
     Persona();
     Persona(string nombre, int edad, string telefono, string correo, string direccion);
     virtual ~Persona();
     
-    // Setters
     void setNombre(string);
     void setEdad(int);
     void setTelefono(string);
     void setCorreo(string);
     void setDireccion(string);
     
-    // Getters
     string getNombre();
     int getEdad();
     string getTelefono();
     string getCorreo();
     
-    // Métodos
     virtual void mostrarDatosPersonales() = 0;
-    
 };
 
-class Dueño : public Persona{
+class Dueño : public Persona {
 private:
     vector<Mascota*> Mascotas;
     
 public:
-    // Constructores y destructor
     Dueño();
-    Dueño(vector <Mascota*>);
+    Dueño(vector<Mascota*>);
     ~Dueño();
     
-    // Métodos
     bool agendarCita(vector<int>);
     void pagar(int);
     void mostrarMascotas();
@@ -62,33 +55,27 @@ public:
     void mostrarDatosPersonales();
 };
 
-class Veterinario : public Persona{
+class Veterinario : public Persona {
 private:
     vector<int> Horarios;
     vector<Dueño*> Clientes;
     double salario;
     
 public:
-    // Constructores y destructor
     Veterinario();
     Veterinario(vector<int>, vector<Dueño*>, double);
     ~Veterinario();
     
-    // Setters
     void setSalario(double);
-    
-    // Getters
     double getSalario();
     
-    // Métodos
     void mostrarHorarios(vector<int>);
     void asignarCliente(Dueño*);
     double clientesAtendidos();
-    vector<Dueño*> mostrarClientes();
-    void recetarMedicina(Mascota*, string);
-    void registrarDiagnostico(Mascota*, string);
-    bool cancelarCita(int);
+    void recetarMedicina(Mascota* m, string medicina);
+    void registrarDiagnostico(Mascota* m, string diagnostico);
+    bool cancelarCita(int hora);
     void mostrarDatosPersonales();
 };
 
-#endif /* Persona_hpp */
+#endif
